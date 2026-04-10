50c6632fe86b1349f168c9ded7696270e81afc02
Manula-dev <manulajayasundara@gmail.com>
2026-04-10 22:49:44 +0530

    convert to express

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
-import { handleCreateUser } from "../controllers/user.controller.js"; // use exact export name
+/*import { handleCreateUser } from "../controllers/user.controller.js"; // use exact export name
 import { handleLogin, handleSignup } from "../controllers/auth.controller.js";
 
-export const authRoutes = {
+export const authRouter = {
   "/register": (req, res) => {
     if (req.method === "POST") return handleSignup(req, res);
     res.writeHead(405, { "Content-Type": "application/json" });
@@ -20,4 +20,15 @@ export const authRoutes = {
     res.end(JSON.stringify({ error: "Method Not Allowed" }));
   },
 
-};
+}; */
+
+import express from "express";
+import { handleLogin, handleSignup } from "../controllers/auth.controller.js";
+
+export const authRouter = express.Router();
+
+//post /auth/signup
+authRouter.post("/signup", handleSignup);
+
+//post /auth/login
+authRouter.post("/login", handleLogin);
