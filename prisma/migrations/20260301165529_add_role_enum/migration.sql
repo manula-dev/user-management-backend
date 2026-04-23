3e70d698f5182e17cbc414ef798d58ef39dcc550
Manula-dev <manulajayasundara@gmail.com>
2026-04-23 23:41:55 +0530

    deploy

--- prisma/migrations/20260301165529_add_role_enum/migration.sql
+++ prisma/migrations/20260301165529_add_role_enum/migration.sql
@@ -1,12 +1,12 @@
 /*
   Warnings:
 
-  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to alter the column `id` on the `user` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `id` on the `User` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
 
 */
 -- AlterTable
-ALTER TABLE `user` DROP PRIMARY KEY,
+ALTER TABLE `User` DROP PRIMARY KEY,
     ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     ADD COLUMN `emailVerified` BOOLEAN NOT NULL DEFAULT false,
     ADD COLUMN `password` VARCHAR(191) NULL,
--- prisma/migrations/20260413162228_add_user_image/migration.sql
+++ prisma/migrations/20260413162228_add_user_image/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `user` ADD COLUMN `image` VARCHAR(191) NULL;
+ALTER TABLE `User` ADD COLUMN `image` VARCHAR(191) NULL;
